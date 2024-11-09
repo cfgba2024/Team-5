8b268f4fac1b88c763d17ce0f628e91c504ca542
july <julimoralezcaviglia@gmail.com>
2024-11-09 15:30:47 -0600

    foro

--- Frontend/FEFrontend/components/foro.jsx
+++ Frontend/FEFrontend/components/foro.jsx
@@ -1,102 +1,66 @@
 import * as React from 'react';
-import { styled } from '@mui/material/styles';
-import Card from '@mui/material/Card';
-import CardHeader from '@mui/material/CardHeader';
-import CardMedia from '@mui/material/CardMedia';
-import CardContent from '@mui/material/CardContent';
-import CardActions from '@mui/material/CardActions';
-import Collapse from '@mui/material/Collapse';
-import Avatar from '@mui/material/Avatar';
-import IconButton, { IconButtonProps } from '@mui/material/IconButton';
-import Typography from '@mui/material/Typography';
+import { Card, CardHeader, CardMedia, CardContent, CardActions, Collapse, Avatar, Typography, IconButton, Grid } from '@mui/material';
 import { red } from '@mui/material/colors';
-import FavoriteIcon from '@mui/icons-material/Favorite';
-import ShareIcon from '@mui/icons-material/Share';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
-import MoreVertIcon from '@mui/icons-material/MoreVert';
 
-export function foro(){
-    const [expanded, setExpanded] = React.useState(false);
-    const handleExpandClick = () => {
-        setExpanded(!expanded);
-    };
-    return (
-        
-        <Card sx={{ maxWidth: 345 }}>
-          <CardHeader
-            avatar={
-              <Avatar sx={{ bgcolor: red[500] }} aria-label="recipe">
-                R
-              </Avatar>
-            }
-            action={
-              <IconButton aria-label="settings">
-                <MoreVertIcon />
-              </IconButton>
-            }
-            title="Shrimp and Chorizo Paella"
-            subheader="September 14, 2016"
-          />
-          <CardMedia
-            component="img"
-            height="194"
-            image="/static/images/cards/paella.jpg"
-            alt="Paella dish"
-          />
-          <CardContent>
-            <Typography variant="body2" sx={{ color: 'text.secondary' }}>
-              This impressive paella is a perfect party dish and a fun meal to cook
-              together with your guests. Add 1 cup of frozen peas along with the mussels,
-              if you like.
-            </Typography>
-          </CardContent>
-          <CardActions disableSpacing>
-            <IconButton aria-label="add to favorites">
-              <FavoriteIcon />
-            </IconButton>
-            <IconButton aria-label="share">
-              <ShareIcon />
-            </IconButton>
-            <ExpandMore
-              expand={expanded}
-              onClick={handleExpandClick}
-              aria-expanded={expanded}
-              aria-label="show more"
-            >
-              <ExpandMoreIcon />
-            </ExpandMore>
-          </CardActions>
-          <Collapse in={expanded} timeout="auto" unmountOnExit>
+export default function Foro() {
+  const [expanded, setExpanded] = React.useState(false);
+
+  const handleExpandClick = () => {
+    setExpanded(!expanded);
+  };
+
+  // Datos de las cards (esto lo puedes modificar según necesites)
+  const cardsData = [
+    { title: "Profesor Agricultura", subheader: "Septiembre 14, 2016", image: "https://th.bing.com/th/id/OIP.QLBfSJEjj8dej_il_dW_rgHaE8?rs=1&pid=ImgDetMain" },
+    { title: "Profesor Porcino", subheader: "Mayo 3, 2023", image: "https://th.bing.com/th/id/OIP.xl9qEEF5KSuu6h3vnDZP3AHaEP?rs=1&pid=ImgDetMain" },
+    { title: "Profesor Bovino", subheader: "Agosto 22, 2022", image: "https://th.bing.com/th/id/OIP.R9TkAiIS08wnXjDgM9MvAAHaE8?rs=1&pid=ImgDetMain" },
+    { title: "Profesor Vitinicultura", subheader: "Abril 11, 2019", image: "https://www.cursocatadelvino.com/wp-content/uploads/2020/10/convertirse-en-un-viticultor-.jpg" },
+    { title: "Profesor Ovino", subheader: "Febrero 18, 2021", image: "https://th.bing.com/th/id/OIP.ufhjVlxdd6PLF-iYgBoe3AHaEb?rs=1&pid=ImgDetMain" },
+    { title: "Profesor Apucultura", subheader: "Junio 7, 2020", image: "https://th.bing.com/th/id/R.9c82c6d1a9850a643afdb1741d4e855a?rik=VpUAZsykz3gdgQ&pid=ImgRaw&r=0" },
+    { title: "Profesor Avícola", subheader: "Enero 15, 2018", image: "https://th.bing.com/th/id/OIP.roK1aSYRjU9oFqvKhxsFkgHaF7?rs=1&pid=ImgDetMain" },
+    { title: "Profesor Agricultura", subheader: "Julio 23, 2019", image: "https://th.bing.com/th/id/OIP.QLBfSJEjj8dej_il_dW_rgHaE8?rs=1&pid=ImgDetMain" },
+    { title: "Profesor Agricultura", subheader: "Diciembre 30, 2022", image: "https://th.bing.com/th/id/OIP.QLBfSJEjj8dej_il_dW_rgHaE8?rs=1&pid=ImgDetMain" }
+  ];
+
+  return (
+    <Grid container spacing={3} justifyContent="center">
+      {/* Mapeamos sobre el array de datos para crear las cards */}
+      {cardsData.map((card, index) => (
+        <Grid item xs={12} sm={6} md={4} key={index}>
+          <Card sx={{ maxWidth: 345 }}>
+            <CardHeader
+              avatar={<Avatar sx={{ bgcolor: red[500] }} aria-label="recipe">R</Avatar>}
+              title={card.title}
+              subheader={card.subheader}
+            />
+            <CardMedia
+              component="img"
+              height="194"
+              image={card.image}
+              alt="Informe"
+            />
             <CardContent>
-              <Typography sx={{ marginBottom: 2 }}>Method:</Typography>
-              <Typography sx={{ marginBottom: 2 }}>
-                Heat 1/2 cup of the broth in a pot until simmering, add saffron and set
-                aside for 10 minutes.
-              </Typography>
-              <Typography sx={{ marginBottom: 2 }}>
-                Heat oil in a (14- to 16-inch) paella pan or a large, deep skillet over
-                medium-high heat. Add chicken, shrimp and chorizo, and cook, stirring
-                occasionally until lightly browned, 6 to 8 minutes. Transfer shrimp to a
-                large plate and set aside, leaving chicken and chorizo in the pan. Add
-                pimentón, bay leaves, garlic, tomatoes, onion, salt and pepper, and cook,
-                stirring often until thickened and fragrant, about 10 minutes. Add
-                saffron broth and remaining 4 1/2 cups chicken broth; bring to a boil.
-              </Typography>
-              <Typography sx={{ marginBottom: 2 }}>
-                Add rice and stir very gently to distribute. Top with artichokes and
-                peppers, and cook without stirring, until most of the liquid is absorbed,
-                15 to 18 minutes. Reduce heat to medium-low, add reserved shrimp and
-                mussels, tucking them down into the rice, and cook again without
-                stirring, until mussels have opened and rice is just tender, 5 to 7
-                minutes more. (Discard any mussels that don&apos;t open.)
-              </Typography>
-              <Typography>
-                Set aside off of the heat to let rest for 10 minutes, and then serve.
+              <Typography variant="body2" sx={{ color: 'text.secondary' }}>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate natus nesciunt voluptatem veritatis placeat minima odit earum accusamus, aliquam ipsam nam excepturi impedit eveniet amet, esse doloremque provident! Suscipit, distinctio!
               </Typography>
             </CardContent>
-          </Collapse>
-        </Card>
-      );
-    
-            
+            <CardActions disableSpacing>
+              <IconButton expand={expanded} onClick={handleExpandClick} aria-expanded={expanded} aria-label="show more">
+                <ExpandMoreIcon />
+              </IconButton>
+            </CardActions>
+            <Collapse in={expanded} timeout="auto" unmountOnExit>
+              <CardContent>
+                <Typography sx={{ marginBottom: 2 }}>Detalles:</Typography>
+                <Typography sx={{ marginBottom: 2 }}>
+                  Detalles adicionales del informe o estudio.
+                </Typography>
+              </CardContent>
+            </Collapse>
+          </Card>
+        </Grid>
+      ))}
+    </Grid>
+  );
 }
